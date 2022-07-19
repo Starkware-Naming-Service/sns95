7d77b8d843e558d1b1160ff6d05395226eb70289
skrider <skrider@berkeley.edu>
2022-07-19 02:44:22 -0400

    encode name as felt done

--- src/utils/felts.ts
+++ src/utils/felts.ts
@@ -1,6 +1,6 @@
-// import { pedersen } from "starknet/dist/utils/hash";
+import { pedersen } from "starknet/dist/utils/hash";
 
-// export const charToFelt = (str: string): number => str.charCodeAt(0);
+export const charToFelt = (str: string): number => str.charCodeAt(0);
 
 // export const encodeStrAsListOfFelts = (input: string): number[] => {
 //   const inputList = input.split("");
@@ -8,10 +8,32 @@
 //   return outputList;
 // };
 
-// export const hashName = (name: string) => {
-//   if (name.length === 0) {
-//     return 0;
-//   }
-//   const recursiveHash = hashName(name.slice(1));
-//   return pedersen([charToFelt(name[0]), recursiveHash]);
-// };
+export const hashName = (name: string) : string => {
+  if (name.length === 0) {
+    return "0";
+  }
+  const recursiveHash = hashName(name.slice(1));
+  return pedersen([charToFelt(name[0]), recursiveHash]);
+};
+
+/*
+  There is an implied .stark. This hash function will only work for small strings.
+*/
+export const encodeNameAsFelt = (name: string) : string => {
+  if (name.length > 30) {
+    throw new Error("name too long")
+  }
+  let length = name.length < 16 ? `0${name.length.toString(16)}` : name.length.toString(16)
+  return [length, ...name.split("").map(char => char.charCodeAt(0).toString(16)), "0".repeat(60)].join("").slice(0, 62)
+};
+
+export const decodeNameAsFelt = (name: string) : string => {
+  let length = parseInt(name.slice(0, 2), 16)
+  let acc = ""
+  for (let i = 0; i < length * 2; i += 2) {
+    console.log(i)
+    console.log(name.slice(i + 2, i + 4))
+    acc = acc.concat(String.fromCharCode(parseInt(name.slice(i + 2, i + 4), 16)))
+  }
+  return acc
+};
